6e43e1011a1eeb8dee12fdc96ad44c76c6cf0e85
Alan Bellows <it@midwifery.edu>
2020-09-02 11:20:15 -0600

    Added flagging-event API endpoint

--- python/api.py
+++ python/api.py
@@ -1,8 +1,6 @@
 
 #import sys
 
-
-
 import os
 import json
 import flask
@@ -20,8 +18,8 @@ faulthandler.enable()
 
 
 with open("../api-config.json") as json_data_file:
-    config = json.load(json_data_file)
-                                                             
+	config = json.load(json_data_file)
+
 # logging.basicConfig(filename='../api.log', filemode='w', level=logging.DEBUG)
 logging.basicConfig(filename='../api.log', filemode='w', level=logging.WARNING)
 
@@ -31,14 +29,20 @@ logging.warning('warning')
 logging.error('error')
 logging.critical('critical')
 
+verbose_mode = True
 app = flask.Flask(__name__)
 db = CivicDB(config.get("mariadb"), logging)
-app.config["DEBUG"] = True
+# app.config["DEBUG"] = True
 
+
+def to_console (message):
+	# return False
+	if (verbose_mode is True):
+		print("[" + str(datetime.now()) + "] " + message)
 
 
 def quit_with_error(title,message,code=500):
-	print(title + ": " + message)
+	to_console(title + ": " + message)
 	#return "<h1>" + title + "</h1><p>" + message + "</p>", code
 	#sys.exit()
 	#abort(code, description=title)
@@ -51,7 +55,6 @@ def quit_with_error(title,message,code=500):
 	}
 	
 	return jsonify(results)
-
 
 
 def random_string(string_length=10):
@@ -98,21 +101,21 @@ def authenticate_user(user_id, password):
 	if password is None: 
 		return quit_with_error("Incomplete Request","Your request did not include all required parameters (password).", 400)
 
-	print("Authenticating user id " + user_id + "...")
+	to_console("Authenticating user id " + user_id + "...")
 	
 	user = get_user_by_id(user_id)
 
 	if user is not None:		
-		ph = PasswordHasher()    
-		# print(user)
+		ph = PasswordHasher()
+		# to_console(user)
 
 		if (ph.verify(user['password_hash'], password)):
 			return user
 		else:
-			print("Incorrect password\n")
+			to_console("Incorrect password\n")
 			return False
 	else:
-		print("User not found\n")
+		to_console("User not found\n")
 		return False
 
 
@@ -137,6 +140,21 @@ def get_locale_id(locale_string):
 	return config['default_locale_id']
 
 
+def get_locale_string(locale_id):
+
+	if locale_id: 
+
+		locale_query = "SELECT * FROM locale WHERE locale_id = %s LIMIT 1"
+		locale       = db.fetchone(locale_query, (locale_id,))
+
+		if (locale and locale['code']):
+			return locale['code']
+		else:
+			logging.debug("Locale not found")
+
+	return False
+
+
 def get_localized_string( string_key, locale_id ):
 	string_query = "SELECT text FROM string WHERE string_key = %s AND locale_id = %s LIMIT 1"
 	string       = db.fetchone(string_query, (string_key, locale_id))
@@ -150,10 +168,17 @@ def get_localized_string( string_key, locale_id ):
 		return "[Error: string not found]";
 
 
-def get_notification_type_name (notification_type_id):
+def get_notification_type_name (flag_type_id):
+	query = "SELECT name FROM flag_type WHERE flag_type_id = %s"
+	row = db.fetchone(query, (flag_type_id,))
+	return row['name']
+
+
+def get_flag_type_name (notification_type_id):
 	query = "SELECT name FROM notification_type WHERE notification_type_id = %s"
 	row = db.fetchone(query, (notification_type_id,))
 	return row['name']
+
 
 
 @app.errorhandler(404)
@@ -163,7 +188,8 @@ def page_not_found(e):
 
 @app.route('/', methods=['GET'])
 def home():
-    return '<h1>Civic API (' + config['version'] + ')</h1>'
+	to_console("home")
+	return '<h1>Civic API (' + config['version'] + ')</h1>'
 
 
 @app.route('/favicon.ico')
@@ -173,6 +199,7 @@ def favicon():
 
 @app.route('/api/v1/register', methods=['GET','POST'])
 def api_register():
+	to_console("register")
 	requestJson = request.form.get("json")
 	timestamp   = datetime.now()	
 	password    = random_string(20)
@@ -247,6 +274,7 @@ def api_register():
 
 @app.route('/api/v1/mission', methods=['POST'])
 def api_mission():
+	to_console("mission")
 	params     = json.loads(request.form.get("json"))
 	mission_id = params.get('mission_id')
 	user_id    = request.form.get("user_id")
@@ -254,11 +282,11 @@ def api_mission():
 	user       = authenticate_user(user_id, password)	
 
 	if mission_id is None:
-		print("missing mission_id\n")
+		to_console("missing mission_id\n")
 		return quit_with_error("Incomplete Request","Your request did not include all required parameters.", 400)
 
 	if user is None or user is False:
-		print("auth failed\n")
+		to_console("auth failed\n")
 		return quit_with_error("Incorrect Login","Your credentials are incorrect.", 401)
 	
 	try:
@@ -290,6 +318,7 @@ def api_mission():
 
 @app.route('/api/v1/flag', methods=['POST'])
 def api_flag():	
+	to_console("flag")
 	# logging.debug(request.form.get("json"))
 	params            = json.loads(request.form.get("json"))
 	url               = params.get("url")
@@ -303,7 +332,7 @@ def api_flag():
 	user              = authenticate_user(user_id, password)
 	flagging_event_id = 0;
 
-	# print(jsonify(flags))	
+	# to_console(jsonify(flags))	
 
 	if url is None:
 		return quit_with_error("Incomplete Request","Your request did not include all required parameters.", 400)
@@ -376,7 +405,7 @@ def api_flag():
 
 @app.route('/api/v1/listcampaigns', methods=['GET'])
 def api_listcampaigns():	
-
+	to_console("listcampaigns")
 	try:
 		campaigns = []
 		campaign_query = "SELECT * FROM campaign WHERE active = 1"
@@ -404,7 +433,8 @@ def api_listcampaigns():
 
 
 @app.route('/api/v1/notifications', methods=['GET','POST'])
-def api_notifications():	
+def api_notifications():
+	to_console("notifications")	
 	# logging.debug(request.form.get("json"))
 	user_id           = request.values.get('user_id')
 	password          = request.values.get('password')
@@ -432,6 +462,7 @@ def api_notifications():
 				'title': get_localized_string( row['title_string_key'], locale_id ),
 				'body': get_localized_string( row['body_string_key'], locale_id ),
 				'message_id': row['message_id'],
+				'flagging_event_id': row['flagging_event_id'],
 				'type': get_notification_type_name(row['notification_type_id'])
 			}
 			notifications.append(notification)
@@ -482,8 +513,8 @@ def api_notifications():
 
 
 @app.route('/api/v1/message-test', methods=['GET','POST'])
-def message_test():	
-
+def api_message_test():	
+	to_console("message_test")
 	user_id   = request.values.get('user_id')
 	password  = request.values.get('password')
 	timestamp = datetime.now()	
@@ -503,11 +534,11 @@ def message_test():
 		message_id = db.lastrowid()
 
 		notification_query = ("INSERT INTO notification"
-			"(notification_type_id, user_id_strict, title_string_key, body_string_key, message_id, timestamp) "
-			"VALUES (%s, %s, %s, %s, %s, %s)"
+			"(flagging_event_id, notification_type_id, user_id_strict, title_string_key, body_string_key, message_id, timestamp) "
+			"VALUES (%s, %s, %s, %s, %s, %s, %s)"
 		)
 
-		db.execute(notification_query, (1, user_id, 'message_from_a_journalist', 'click_here_to_read', message_id, datetime.now()))
+		db.execute(notification_query, (818, 1, user_id, 'message_from_a_journalist', 'click_here_to_read', message_id, datetime.now()))
 
 		results = {
 			'status': 'success'
@@ -530,6 +561,7 @@ def message_test():
 
 @app.route('/api/v1/message', methods=['GET','POST'])
 def api_message():	
+	to_console("message")
 	requestJson = request.values.get("json")
 	user_id     = request.values.get('user_id')
 	password    = request.values.get('password')
@@ -563,6 +595,79 @@ def api_message():
 				}
 
 				db.execute("UPDATE message SET viewed = %s WHERE user_id = %s AND message_id = %s", (datetime.now(),user_id,message_id))
+
+				return jsonify(results)
+
+			else:
+
+				return page_not_found(False)		
+
+		else:
+			results = {
+				'status': 'error',
+				'message': "Error: Missing json parameter",				
+			}			
+
+	except mysql.connector.errors.DatabaseError as err:
+
+		results = {
+			'error': "Error: {}".format(err),
+			'status': 'error',
+			'message': 'Database error'
+		}
+
+		return jsonify(results)
+
+
+@app.route('/api/v1/flagging-event', methods=['GET','POST'])
+def api_flagging_event():	
+	to_console("flagging_event")
+	requestJson = request.values.get("json")
+	user_id     = request.values.get('user_id')
+	password    = request.values.get('password')
+	user        = authenticate_user(user_id, password)
+	results     = {}
+
+	if user is None or user is False:
+		return quit_with_error("Incorrect Login","Your credentials are incorrect.", 401)
+	
+	try:
+
+		if (requestJson):
+			params = json.loads(requestJson)
+			flagging_event_id = params.get("flagging_event_id")
+
+			flagging_event_query = "SELECT * FROM flagging_event WHERE user_id = %s AND flagging_event_id = %s"
+			flagging_event_query_data = (user_id, flagging_event_id)
+
+			flagging_event = db.fetchone(flagging_event_query, flagging_event_query_data)
+
+			if flagging_event:
+
+				results = {
+					'status': 'success',
+					'flagging_event': {
+						'flagging_event_id': flagging_event['flagging_event_id'],
+						'user_id': flagging_event['user_id'],
+						'locale': get_locale_string(flagging_event['locale_id']),
+						'url': flagging_event['url'],
+						'notes': flagging_event['notes'],
+						'campaign': flagging_event['campaign'],
+						'flags': []
+					}
+				}
+
+				flag_query = "SELECT * FROM flag WHERE flagging_event_id = %s"
+				flag_query_data = (flagging_event_id,)
+
+				flags = db.fetchall(flagging_event_query, flagging_event_query_data)
+
+				for flag in flags:
+					flag_details = {
+						'type': get_notification_type_name(flag['flag_type_id']),
+						'severity': flag['severity']
+					}
+					results.flagging_event.flags.append(flag_details)
 
 				return jsonify(results)
 
